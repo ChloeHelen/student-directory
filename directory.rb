c570f42249534d80d993195fe590b282d3b993d0
Chloe Mingay <cmingay@fastmail.fm>
2018-08-24 13:30:52 +0000

    Program adds more information: Hobbies, nationality and favourite colour

--- directory.rb
+++ directory.rb
@@ -1,17 +1,24 @@
 def input_students
-    puts "Please enter the names of the students"
+    puts "Please enter the names of the students followed by their information"
     puts "To finish, just hit return twice"
     #create an empty array
     students = []
     #get the first name
-    name = gets.chomp
+    name = gets.chomp.capitalize
     #while the name is not empty, repeat this code
     while !name.empty? do
+        puts "What is #{name}'s favourite hobby?"
+        hobby = gets.chomp.downcase
+        puts "What's their nationality?"
+        nationality = gets.chomp.capitalize
+        puts "What's their favourite colour?"
+        colour = gets.chomp.downcase
         #add the student hash to the array
-        students << {name: name, cohort: :november}
+        students << {name: name, cohort: :november, hobby: hobby, nationality: nationality, colour: colour}
         puts "Now we have #{students.count} students"
         #get another name from the user
-        name = gets.chomp
+        puts "Enter another student's name or press return to exit"
+        name = gets.chomp.capitalize
     end
     # return the array of students
     students
@@ -23,10 +30,11 @@ def print_header
 end
 
 def print(students)
-    index = 0
-    while index < students.length
-        puts "#{students[index][:name]} (#{students[index][:cohort]} cohort)"
-        index += 1 
+    students.each do |student|
+        
+        if student[:name].length < 12
+            puts "#{student[:name]} (#{student[:cohort]} cohort). They like #{student[:hobby]}, they're #{student[:nationality]} and their favourite colour is #{student[:colour]}."
+        end
     end
 end
 
