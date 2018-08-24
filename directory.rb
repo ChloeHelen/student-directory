d8e2611de54a578de92f2a52f8354b659f49f1cb
Chloe Mingay <cmingay@fastmail.fm>
2018-08-24 15:04:15 +0000

    Assigns standard value for empty cohort and checks if cohort is a valid month

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 def input_students
     puts "Please enter the names of the students followed by their information"
     puts "To finish, just hit return twice"
@@ -7,6 +9,17 @@ def input_students
     name = gets.chomp.capitalize
     #while the name is not empty, repeat this code
     while !name.empty? do
+        puts "What cohort is #{name} a part of?"
+        cohort = gets.chomp.downcase.capitalize
+        #If cohort is left empty it is assigned a standard value
+        if cohort.empty?
+            cohort = "August"
+        end
+        #Uses date library to check whether the cohort entered is a valid month
+        while !Date::MONTHNAMES.include?(cohort)
+            puts "Invalid input, please enter a month. e.g. 'January'or 'February'"
+            cohort = gets.chomp
+        end
         puts "What is #{name}'s favourite hobby?"
         hobby = gets.chomp.downcase
         puts "What's their nationality?"
@@ -14,7 +27,7 @@ def input_students
         puts "What's their favourite colour?"
         colour = gets.chomp.downcase
         #add the student hash to the array
-        students << {name: name, cohort: :november, hobby: hobby, nationality: nationality, colour: colour}
+        students << {name: name, cohort: cohort.to_sym, hobby: hobby, nationality: nationality, colour: colour}
         puts "Now we have #{students.count} students"
         #get another name from the user
         puts "Enter another student's name or press return to exit"
@@ -31,10 +44,7 @@ end
 
 def print(students)
     students.each do |student|
-        
-        if student[:name].length < 12
             puts "#{student[:name]} (#{student[:cohort]} cohort). They like #{student[:hobby]}, they're #{student[:nationality]} and their favourite colour is #{student[:colour]}."
-        end
     end
 end
 
