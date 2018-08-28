5f37678fae26214fb7efc06d6aefdabea956dd58
Chloe Mingay <cmingay@fastmail.fm>
2018-08-28 10:39:25 +0000

    Uses strip() to get rid of the last return character

--- directory.rb
+++ directory.rb
@@ -10,7 +10,7 @@ def input_students
     #while the name is not empty, repeat this code
     while !name.empty? do
         puts "What cohort is #{name} a part of?"
-        cohort = gets.chomp.downcase.capitalize
+        cohort = gets.strip.downcase.capitalize
         #If cohort is left empty it is assigned a standard value
         if cohort.empty?
             cohort = "August"
@@ -18,20 +18,20 @@ def input_students
         #Uses date library to check whether the cohort entered is a valid month
         while !Date::MONTHNAMES.include?(cohort)
             puts "Invalid input, please enter a month. e.g. 'January'or 'February'"
-            cohort = gets.chomp
+            cohort = gets.strip
         end
         puts "What is #{name}'s favourite hobby?"
-        hobby = gets.chomp.capitalize
+        hobby = gets.strip.capitalize
         puts "What's their nationality?"
-        nationality = gets.chomp.capitalize
+        nationality = gets.strip.capitalize
         puts "What's their favourite colour?"
-        colour = gets.chomp.capitalize
+        colour = gets.strip.capitalize
         #add the student hash to the array
         students << {name: name, cohort: cohort.to_sym, hobby: hobby, nationality: nationality, colour: colour}
         puts "Now we have #{students.count} students"
         #get another name from the user
         puts "Enter another student's name or press return to exit"
-        name = gets.chomp.capitalize
+        name = gets.strip.capitalize
     end
     # return the array of students
     students
