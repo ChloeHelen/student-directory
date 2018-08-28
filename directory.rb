d9deb24056a25a2352a4a9c7e88e25c9670b25dc
Chloe Mingay <cmingay@fastmail.fm>
2018-08-28 13:20:36 +0000

    Adds interactive menu

--- directory.rb
+++ directory.rb
@@ -75,9 +75,26 @@ def print_footer(names)
     end
 end
 
-students = input_students
-if students.count > 0
-    print_header
-    print_by_cohorts(students)
-    print_footer(students)
+def interactive_menu
+    students = []
+    loop do
+        puts "1. Input the students"
+        puts "2. Show the students"
+        puts "9. Exit"
+        selection = gets.chomp
+        case selection
+            when "1"
+                students = input_students
+            when "2"
+                print_header
+                print(students)
+                print_footer(students)
+            when "9"
+                exit
+            else
+                puts "I don't know what you mean, try again"
+        end
+    end
 end
+
+interactive_menu
